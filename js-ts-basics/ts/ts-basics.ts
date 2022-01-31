86a5e6858350cd5f10ac402472b174dc4c5a4033
aniruddhha <aniruddha.kudalkar@gmail.com>
2022-01-31 12:48:04 +0530

    typescript : ts basics

--- js-ts-basics/ts/ts-basics.ts
+++ js-ts-basics/ts/ts-basics.ts
@@ -3,8 +3,8 @@ const dt: number = 10
 let tm = 'abc'
 // tm = new Date()
 const isOkay: boolean = true
-const fn: () => void = function(): void {
-//    return new Date()
+const fn: () => void = function (): void {
+    //    return new Date()
 }
 
 // const ab : void = 'jhkjhjk'
@@ -13,5 +13,14 @@ function wowTs(): string {
     return 'some string'
 }
 
-const fnArrow: () => void = () =>  console.log('hello')
-const fnArrowOne: (num : string) => number = (num : string) =>  100
+const fnArrow: () => void = () => console.log('hello')
+const fnArrowOne: (num: string) => number = (num: string) => 100
+
+const anything: any = (num: string) => 100
+const mixed: boolean | (() => void) = () => console.log('Okay')
+
+const obj: { [key : string] : number|string } = {
+    age: 10,
+    'os': 'abc',
+    'url-staging': 'abc.com'
+}
