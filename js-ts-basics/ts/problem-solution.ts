e5c6f3575ec148ab4c94fe7f9ebf3ce4a5f1558c
aniruddhha <aniruddha.kudalkar@gmail.com>
2022-01-31 12:28:16 +0530

    typescript : basics of type

---
+++ js-ts-basics/ts/problem-solution.ts
@@ -0,0 +1,8 @@
+// solution to problem
+function solutionToDate() {
+
+    let dt: Date = new Date()
+    // dt = true
+
+    return dt
+}
---
+++ js-ts-basics/ts/ts-basics.ts
@@ -0,0 +1,17 @@
+
+const dt: number = 10
+let tm = 'abc'
+// tm = new Date()
+const isOkay: boolean = true
+const fn: () => void = function(): void {
+//    return new Date()
+}
+
+// const ab : void = 'jhkjhjk'
+
+function wowTs(): string {
+    return 'some string'
+}
+
+const fnArrow: () => void = () =>  console.log('hello')
+const fnArrowOne: (num : string) => number = (num : string) =>  100
