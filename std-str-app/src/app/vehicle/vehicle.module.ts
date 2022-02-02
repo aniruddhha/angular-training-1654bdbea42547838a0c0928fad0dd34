d2a3b3b3873c7a58d7a3a54cf673c651ecdace6b
aniruddhha <aniruddha.kudalkar@gmail.com>
2022-02-02 12:18:26 +0530

    angular : module :setup

--- std-str-app/src/app/vehicle/vehicle.module.ts
+++ std-str-app/src/app/vehicle/vehicle.module.ts
@@ -3,7 +3,9 @@ import { CommonModule } from '@angular/common';
 import { VehicleComponent } from './vehicle.component';
 
 
-
+// --flat say --> do not create new folder and create in same folder
+// ng g name of module/name of component --flat
+// ng g m name of module
 @NgModule({
   declarations: [
     VehicleComponent
